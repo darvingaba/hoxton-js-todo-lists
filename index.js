0aaaa8e103c9c7f5e2e806acb84eace68915f80e
darvingaba <darvingaba1@gmail.com>
2022-06-16 23:19:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+// !- Create an alert that lists all users, with their ids, names and what city they're from
+let credentials=[]
+
+for (let i = 0; i < users.length; i++) {
+    credentials.push(
+        `${users[i].id} - ${users[i].name} - ${users[i].address.city}\n`,
+    )
+}
+alert(credentials)
+
+
+// !- Prompt the user for a user id
+let userId = Number(prompt("Enter a user id"))
+
+
+// !- Display an alert with the username and all the todos titles that belong to that user 
+let userAndTodos = []
+
+for (let name in users) {
+    if (users[name].id === userId) {
+        userAndTodos.push(
+            `${users[name].name}:\n`,
+        )
+    }
+}
+    
+for(let todo in todos){
+    if(todos[todo].userId === userId){
+        userAndTodos.push(
+            `${todos[todo].title}\n`,
+        )
+    }
+}
+
+console.log(userAndTodos)
+
+alert(userAndTodos)
+
+
+// !Tips
+// !- The user id creates an identifiable relationship with the todos
+// !- Sometimes it's good to map some values into a new data structure
+
+// !Challenge
+// !After you select a user, add the option to either show the todos or add a new todo to the list
+
+// !Challenge 2
+// !Now that you can add a todo, add the option to either delete or update a todo. Add also the option to repeatedly choose a different user, or to finish the program
